d56e3f282b3826e2712c55cde61d48b47ed94fdf
Nathanael Ledrich <nathanael.ledrich@icloud.com>
2021-10-30 22:13:44 +0200

    add footer

--- spodaily/settings.py
+++ spodaily/settings.py
@@ -41,9 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'spodaily_api.apps.SpodailyApiConfig',
-    'rest_framework',
-    'corsheaders',
+    'spodaily_api.apps.SpodailyApiConfig'
 ]
 
 MIDDLEWARE = [
@@ -84,9 +82,9 @@ WSGI_APPLICATION = 'spodaily.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
-        'NAME': 'spodaily', # le nom de notre base de donnees creee precedemment
+        'NAME': 'nledrich', # le nom de notre base de donnees creee precedemment
         'USER': 'postgres', # attention : remplacez par votre nom d'utilisateur
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '5433',
 
