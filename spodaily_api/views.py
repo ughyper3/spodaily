8c6813279b187359fec0900a70003f0b908b1559
ughyper3 <72391421+ughyper3@users.noreply.github.com>
2021-11-11 13:42:59 +0100

    refactor slices

--- spodaily_api/views.py
+++ spodaily_api/views.py
@@ -221,7 +221,7 @@ class DeleteActivityView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy('past_session')
 
     def get(self, request, *args, **kwargs):
-        activity_uuid = request.get_full_path()[26:-1]
+        activity_uuid = kwargs['fk']
         session = Session.objects.filter(activity_session_id=activity_uuid).values('name', 'date')[0]
         activity = Activity.objects.filter(uuid=activity_uuid).values('exercise_id__name')[0]
         context = {'session': session, 'activity': activity}
@@ -234,7 +234,7 @@ class DeleteFutureActivityView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy('routine')
 
     def get(self, request, *args, **kwargs):
-        activity_uuid = request.get_full_path()[33:-1]
+        activity_uuid = kwargs['fk']
         session = Session.objects.filter(activity_session_id=activity_uuid).values('name', 'date')[0]
         activity = Activity.objects.filter(uuid=activity_uuid).values('exercise_id__name')[0]
         context = {'session': session, 'activity': activity}
@@ -247,7 +247,7 @@ class DeleteProgramActivityView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy('program')
 
     def get(self, request, *args, **kwargs):
-        activity_uuid = request.get_full_path()[34:-1]
+        activity_uuid = kwargs['fk']
         session = Session.objects.filter(activity_session_id=activity_uuid).values('name', 'date')[0]
         activity = Activity.objects.filter(uuid=activity_uuid).values('exercise_id__name')[0]
         context = {'session': session, 'activity': activity}
@@ -267,7 +267,7 @@ class MuscleView(LoginRequiredMixin, TemplateView):
     template_name = 'spodaily_api/muscle.html'
 
     def get(self, request, *args, **kwargs):
-        uuid = request.get_full_path()[17:-1]  # disgusting way to get url
+        uuid = kwargs['fk']  # disgusting way to get url
         muscle = get_muscle_by_uuid(uuid)
         exercise = get_exercise_by_muscle(uuid)
         context = {'muscle': muscle,
@@ -492,7 +492,7 @@ class DuplicateProgramSessionView(LoginRequiredMixin, TemplateView):
     template_name = "spodaily_api/duplicate_program_session.html"
 
     def get(self, request, *args, **kwargs):
-        session_uuid = request.get_full_path()[36:-1]
+        session_uuid = kwargs['fk']
         session = Session.objects.get(uuid=session_uuid)
         form = AddSessionDuplicateForm()
         context = {'session': session,
@@ -503,7 +503,7 @@ class DuplicateProgramSessionView(LoginRequiredMixin, TemplateView):
         form = AddSessionDuplicateForm(request.POST)
         form.instance.user = request.user
         if form.is_valid():
-            session_uuid = request.get_full_path()[36:-1]
+            session_uuid = kwargs['fk']
             session = Session.objects.get(uuid=session_uuid)
             activities = Activity.objects.filter(session_id=session_uuid)
             session_2 = session
@@ -525,7 +525,7 @@ class MarkSessionAsDone(LoginRequiredMixin, TemplateView):
     template_name = "spodaily_api/session_done.html"
 
     def get(self, request, *args, **kwargs):
-        session_uuid = request.get_full_path()[23:-1]
+        session_uuid = kwargs['fk']
         session = Session.objects.get(uuid=session_uuid)
         form = SessionDoneForm()
         context = {'form': form,
@@ -537,7 +537,7 @@ class MarkSessionAsDone(LoginRequiredMixin, TemplateView):
         form = SessionDoneForm(request.POST)
         form.instance.user = request.user
         if form.is_valid():
-            session_uuid = request.get_full_path()[23:-1]
+            session_uuid = kwargs['fk']
             session = Session.objects.get(uuid=session_uuid)
             session.is_done = True
             session.save()
