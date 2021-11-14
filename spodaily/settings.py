749fda083c8a34946f1f6f8c1c7c8032e01d6d32
ughyper3 <72391421+ughyper3@users.noreply.github.com>
2021-11-14 09:51:00 +0100

    correct unit test

--- spodaily/settings.py
+++ spodaily/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qtg6s+ixh65gn+8nx2-*$4iyqz&e=t$wz)-mpk=%+@d^mfcx5v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS =['*']
 
